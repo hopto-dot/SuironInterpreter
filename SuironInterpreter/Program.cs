2114062b5ac774cb99e060d8beeeafe629982d1f
hopto-dot <66906618+hopto-dot@users.noreply.github.com>
2025-02-12 14:59:49 +0000

    Add project files.

---
+++ SuironInterpreter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+
+namespace SuironInterpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: SuironInterpreter [script]");
+                return;
+            }
+            else if (args.Length == 1)
+            {
+                runFile(args[0]);
+            }
+            else
+            {
+                runPrompt();
+            }
+        }
+
+        private static void runFile(String path)
+        {
+            String sourceText = File.ReadAllText(path);
+            Run(sourceText);
+        }
+
+        private static void Run(String source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.scanTokens();
+
+            foreach (Token token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        private static void runPrompt()
+        {
+
+        }
+    }
+}
---
+++ SuironInterpreter/Scanner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuironInterpreter
+{
+    class Scanner
+    {
+        public Scanner (String source)
+        {
+
+        }
+
+        public List<Token> scanTokens()
+        {
+            return new List<Token>();
+        }
+
+    }
+}
