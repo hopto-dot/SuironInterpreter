963b3d584fbcd12d2c0d7510270c09969e6e1ca9
hopto-dot <66906618+hopto-dot@users.noreply.github.com>
2025-03-27 10:59:06 +0000

    Change token comment placement

--- SuironInterpreter/Token.cs
+++ SuironInterpreter/Token.cs
@@ -8,19 +8,19 @@ namespace SuironInterpreter
 {
     public class Token
     {
-        public TokenType Type { get; }
-        public string Lexeme { get; }
-        public object Literal { get; }
-        public int Line { get; }
+        public TokenType Type { get; } // TokenType enum value - for example LEFT_PAREN, SEMICOLON or STRING
+        public string Lexeme { get; } // The actual token text from the source code
+        public object Literal { get; } // The parsed value as opposed to text form (for strings, numbers, etc.)
+        public int Line { get; } // Line no code where token appears
 
         public Token(TokenType type, string lexeme, object literal, int line)
         {
             // Example class instance: Token(TokenType.NUMBER, "123.45", 123.45, 1)
 
-            Type = type; // TokenType enum value - for example LEFT_PAREN, SEMICOLON or STRING
-            Lexeme = lexeme; // The actual token text from the source code
-            Literal = literal;  // The parsed value as opposed to text form (for strings, numbers, etc.)
-            Line = line; // Line no code where token appears
+            Type = type; 
+            Lexeme = lexeme; 
+            Literal = literal;  
+            Line = line; 
         }
 
         public override string ToString()
